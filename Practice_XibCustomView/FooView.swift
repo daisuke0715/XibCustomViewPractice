38576b13463b7c763c3a6776da3575929b77ec19
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-03-27 17:08:51 +0900

    practice-xibcustomview

---
+++ Practice_XibCustomView/FooView.swift
@@ -0,0 +1,33 @@
+//
+//  FooView.swift
+//  Practice_XibCustomView
+//
+//  Created by 河村大介 on 2021/03/27.
+//
+
+import UIKit
+
+final class FooView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.loadNib()
+        
+    }
+    
+    fileprivate func loadNib() {
+        guard let fooView = UINib(nibName: "FooView", bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView else{
+            return
+        }
+        
+        fooView.frame = self.bounds
+        
+        self.addSubview(fooView)
+    }
+    
+}
---
+++ Practice_XibCustomView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Practice_XibCustomView
+//
+//  Created by 河村大介 on 2021/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
